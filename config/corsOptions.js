c23cc1378d442810c4798a73b48b58c38ae868f5
Divyanshu Singh <118059725+div0021@users.noreply.github.com>
2023-02-12 11:07:15 +0530

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,12 +2,15 @@ const allowedOrigin=require("./allowedOrigin");
 
 const corsOptions={ // this is the third party middle ware.//lookup object.
     origin:(origin,callback)=>{ 
+        console.log("in Cors");
         if(allowedOrigin.indexOf(origin) !== -1 || !origin){
+            console.log("Successful");
             // no origin means that if we test it in postmen or thunder client, they don't have origin.
             callback(null,true) 
             // callback syntax: - callback(error,allow or not);
 
         }else{
+            console.log("reject");
             callback(new Error('Not allowed by CORS'));
         }
     },
