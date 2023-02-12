cf2c0262d55dfd5aabecce0d9bd234111145a934
Divyanshu Singh <118059725+div0021@users.noreply.github.com>
2023-02-12 10:53:21 +0530

    Update allowedOrigin.js

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,8 +1,8 @@
-const allowOrigins=[
+const allowedOrigin=[
     'https://technotes-qesk.onrender.com/',
     'https://technotes-api.onrender.com/'
     
 ]
 //These will be the local host that we accept to access our local api.
 
-module.exports=allowOrigins;
+module.exports=allowedOrigin;
